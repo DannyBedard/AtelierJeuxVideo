8d10a2b61f0e0fc73841226fa4e235982f5ea0d3
TarzanKeyz <dannybedard1@gmail.com>
2021-09-13 11:47:54 -0400

    Affichage de screensaver

--- Engine3D.hpp
+++ Engine3D.hpp
@@ -4,6 +4,8 @@
 #include "Engine.hpp"
 #include "GLContext.hpp"
 #include "GameEvent.hpp"
+#include "Matrix44D.hpp"
+#include "Vector3D.hpp"
 
 namespace TIE{
     class Engine3D : public Engine<Engine3D>{
@@ -12,16 +14,51 @@ namespace TIE{
 
         public:
         void Start(){
+            Matrix44D orthogonalProjection;
+            orthogonalProjection.LoadOrthogonal(640.0, 480.0);
             bool isOpen = true;
+            Vector3D vector = Vector3D(0.0, 0.0, 0.0);
+            double reboundX = 0.05;
+            double reboundY = 0.05;
+            double size = 100.0;
             while(isOpen){
+
+                if(vector.x + size >= glContext.GetSize().x || vector.x < 0.0)
+                    reboundX = -reboundX;
+                if(vector.y + size >= glContext.GetSize().y || vector.y < 0.0)
+                    reboundY = -reboundY;
+                vector.x += reboundX;
+                vector.y += reboundY;
+
                 while(GameEvent::Poll()){
                     switch(GameEvent::GetType()){
                         case SDL_QUIT: 
                             isOpen = false;
                             break;
+                        case SDL_KEYDOWN:
+                            glColor3d(0.0, 0.0, 1.0);
+                            break;
+                        case SDL_KEYUP:
+                            glColor3d(1.0, 1.0, 1.0);
+                            break;
+
                     }
-                    glContext.Refresh();
                 }
+                    glMatrixMode(GL_PROJECTION);
+                    glLoadIdentity();
+                    glMultMatrixd(orthogonalProjection.matrix);
+                    glMatrixMode(GL_MODELVIEW);
+                    glLoadIdentity();
+
+                    glClear(GL_COLOR_BUFFER_BIT);
+                    glBegin(GL_QUADS); 
+                        glVertex3d(vector.x, vector.y, vector.z);
+                        glVertex3d(vector.x + size, vector.y, vector.z);
+                        glVertex3d(vector.x + size, vector.y + size, vector.z);
+                        glVertex3d(vector.x, vector.y + size, vector.z);
+                    glEnd();
+                    glContext.Refresh(); 
+                
             }
         }
     };
---
+++ Matrix44D.hpp
@@ -0,0 +1,40 @@
+#define m11 matrix[0]
+#define m12 matrix[1]
+#define m13 matrix[2]
+#define m14 matrix[3]
+#define m21 matrix[4]
+#define m22 matrix[5]
+#define m23 matrix[6]
+#define m24 matrix[7]
+#define m31 matrix[8]
+#define m32 matrix[9]
+#define m33 matrix[10]
+#define m34 matrix[11]
+#define m41 matrix[12]
+#define m42 matrix[13]
+#define m43 matrix[14]
+#define m44 matrix[15]
+
+
+namespace TIE {
+    class Matrix44D{
+    public:
+        double matrix[16];
+
+        void LoadIdentity(){
+            m11 = m22 = m33 = m44 = 1.0;
+            m12 = m13 = m14 = m21 = m23 = m24 = m31 = m32 = m34 = m41 = m42 = m43 = 0.0;
+        }
+
+        void LoadOrthogonal(double right, double bottom){
+            m11 = 2.0 / right;
+            m22 = 2.0 / -bottom;
+            m33 = m41 = -1.0;
+            m42 = m44 = 1;
+            m12 = m13 = m14 = m21 = m23 = m24 = m31 = m32 = m34 = m43 = 0.0;
+        }
+
+        //Matrice * vecteur
+        //Matrice Rotation
+    };
+}
